37b7805b8276e2ee0da80530e4f02d84e6197186
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-13 00:12:35 -0300

    Feat: Criar método para adicionar um número aleatório de 2 ou 4

--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -10,5 +10,21 @@ public class Tabuleiro {
         random = new Random();
     }
 
+    public void inciarJogo(){
+        adicionarNovoNumero();
+        adicionarNovoNumero();
+    }
+
+    private void adicionarNovoNumero(){
+        int linha, coluna;
+        do {
+            linha = random.nextInt(TAMANHO);
+            coluna = random.nextInt(TAMANHO);
+        } while (tabuleiro[linha][coluna] != 0);
+        tabuleiro[linha][coluna] = random.nextInt(10) == 0 ? 4 : 2;
+    }
+
+
+
 
 }
