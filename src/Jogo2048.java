a47e957c3a4f776e42d6a532309999953004f938
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-12 23:46:17 -0300

    Iniciando projeto de jogo2048

---
+++ src/Jogo2048.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Jogo2048 {
+    private Tabuleiro tabuleiro;
+
+    public Game2048() {
+        tabuleiro = new Tabuleiro();
+        tabuleiro.iniciarJogo();
+    }
+
+
+}
